730e86ffbcd3e8b1f73658f486f17e5b154fc81e
XueyangSong <xus023@ucsd.edu>
2019-10-26 19:54:17 -0700

    first round filter recipe

--- grep.js
+++ grep.js
@@ -1,54 +1,108 @@
-let header = document.querySelector('header');
-let section = document.querySelector('section');
 
-
-let requestURL = './recipe.json';
-
-let request = new XMLHttpRequest();
-
-request.open('GET', requestURL);
-
-request.responseType = 'text';
-request.send();
-
-request.onload = function() {
-	let superHeroesText = request.response;
-	let superHeroes = JSON.parse(superHeroesText);
-	// let superHeroes = request.response;
-	populateHeader(superHeroes);
-	showHeroes(superHeroes);
-};
-
-function showHeroes(jsonObj) {
-	//用heroers存储json文件里menbers的信息
-	let heroes = jsonObj['members'];
-
-	for (let i = 0; i < heroes.length; i++) {
-		//创建一系列页面元素
-		let myArticle = document.createElement('article');
-		let myH2 = document.createElement('h2');
-		let myPara1 = document.createElement('p');
-		let myPara2 = document.createElement('p');
-		let myPara3 = document.createElement('p');
-		let myList = document.createElement('ul');
-
-		myH2.textContent = heroes[i].name;
-		myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-		myPara2.textContent = 'Age: ' + heroes[i].age;
-		myPara3.textContent = 'Superpowers:';
-
-		let superPowers = heroes[i].powers;
-		for(let j = 0; j < superPowers.length; j++) {
-			let listItem = document.createElement('li');
-			listItem.textContent = superPowers[j];
-			myList.appendChild(listItem);
-		}
-
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myList);
-            section.appendChild(myArticle);
-        }
+/*
+ * The first argument is the list of ingredients in the recipe in our database.
+ * The second argument is the list of ingredients that the user has.
+ */
+function compareIngredient(recipeIngre, userIngre) {
+	var i;
+	for (i = 0; i < userIngre.length; i++) {
+	  if (userIngre.includes(recipeIngre[i]) == false) return 0;
 	}
+	return 1;
+}
+
+/*
+ * The first argument is the list of tags in the recipe in our database.
+ * The second argument is the list of tags that the user has.
+ */
+function compareTag(recipeTag, userTag) {
+	var i;
+	for (i = 0; i < userTag.length; i++) {
+		if (recipeTag.includes(userTag[i]) == false) return 0;
+	}
+	return 1;
+}
+
+
+/*
+ * jsonObj: the json object storing the recipe
+ * requirements: user input. Format:
+ * {
+ *   "ingredients": [],
+ *   "calories": "",
+ *   "tags": []
+ * }
+ */
+function selectRecipe(jsonObj, requirements) {
+	// All recipes.
+	let allRecipes = jsonObj['recipes'];
+
+	// All ingredients.
+	let ingredients = requirements['ingredients'];
+	// All tags.
+	let tags = requirements['tags'];
+	// Selection of calory.
+	let calories = requirements['calories'];
+
+	// A list to store all available recipes after the first round of selection:
+	// looking for ingredients.
+	var firstRoundList = [];
+
+	let i = 0;
+
+	// Traverse the list of recipes and filter ingredients.
+	for (i = 0; i < allRecipes.length; i++) {
+		var recipe = allRecipes[i];
+		if (compareIngredient(recipe['ingredients'], ingredients) == 1) {
+			firstRoundList = firstRoundList.concat(recipe);
+		}
+	}
+	if (firstRoundList.length == 0) return [];
+
+	// Start the second round, matching tags.
+	var secondRoundList = [];
+
+	// Traverse the list of tags and filter ingredients.
+	for (i = 0; i < firstRoundList.length; i++) {
+		var recipe = firstRoundList[i];
+		if (compareTag(recipe['tags'], tags) == 1) {
+			secondRoundList = secondRoundList.concat(recipe);
+		}
+	}
+	if (secondRoundList.length == 0) return [];
+
+	// Start the third round.
+	var low = [];
+	var medium = [];
+	var high = [];
+
+	// Firstly, sort the recipe based on calory from the resulted list.
+	for (i = 0; i < secondRoundList.length; i++) {
+		var recipe = secondRoundList[i];
+		if (recipe['calories'].toUpperCase() == "LOW") {
+			low.concat(recipe);
+		}
+		else if (recipe['calories'].toUpperCase() == "MEDIUM") {
+			medium.concat(recipe);
+		}
+		else {
+			high.concat(recipe);
+		}
+	}
+
+	// Then, get the list based on user's preference.
+	var retList = [];
+	if (calories.toUpperCase() == "LOW") {
+		retList.concat(low, medium);
+	}
+	else if (calories.toUpperCase() == "MEDIUM") {
+		retList.concat(medium, low, high);
+	}
+	else {
+		retList.concat(high, medium);
+	}
+
+	return retList;
+
+
+}
